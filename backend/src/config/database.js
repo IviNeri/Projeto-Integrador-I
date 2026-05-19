8b13fd1120c8f1d5f8c97afc5c28996fbe125be4
Carlos Eduardo Aguiar Ramos <cadu17052006@gmail.com>
2026-05-19 20:31:10 -0300

    refactor: ajusta conexão ao banco de dados para comportar SSL

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -5,7 +5,15 @@ const connection = mysql.createPool({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = connection;
